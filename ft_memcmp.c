8ff3867c3b794accc7b92dc071bd87f297bedc83
Maria Teresa Tavares De Sousa Mota <matavare@c1r9s6.42lisboa.com>
2022-11-03 18:15:18 +0000

    mmmmm

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: matavare <matavare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 17:15:37 by matavare          #+#    #+#             */
-/*   Updated: 2022/11/03 17:01:31 by matavare         ###   ########.fr       */
+/*   Created: 2022/11/03 18:03:40 by matavare          #+#    #+#             */
+/*   Updated: 2022/11/03 18:05:43 by matavare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
-
-	i = 0;
-	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	if (!dest && !src)
-		return (NULL);
-	if (dest > src)
-	{
-		while (n > 0)
-		{
-			d[n - 1] = s[n - 1];
-			n--;
-		}
-	}
-	else
-		ft_memcpy(d, s, n);
-	return (dest);
+	
 }
